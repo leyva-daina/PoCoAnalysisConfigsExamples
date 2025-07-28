5a00a7d5e9abca7be110c5a2788b39f8e107fe60
leyva-daina <daina.leyva.pernia@desy.de>
2025-07-28 22:02:11 +0200

    commands to run

--- configs/zmumu/run_commands.sh
+++ configs/zmumu/run_commands.sh
@@ -14,7 +14,8 @@ pocket-coffea run --test --cfg example_config.py --ignore-grid-certificate --cus
 
 # Now, before submitting to condor, you will have to change the NAF executor python configuration from Pocket-Coffea
 # This is mainly because a few changes are needed to be able to work without a GRID certificate
-# You can use the one here
+# You can use the one here https://github.com/leyva-daina/PocketCoffea/blob/Summies25/pocket_coffea/executors/executors_DESY_NAF.py
+# Copy also these custom run options from https://github.com/leyva-daina/PoCoAnalysisConfigsExamples/blob/main/configs/zmumu/my_run_options.yaml
 
 # Submit to condor a test
 pocket-coffea run --test --cfg example_config.py  --ignore-grid-certificate --process-separately --executor parsl-condor@DESY_NAF --custom-run-options my_run_options.yaml  -o output_dask_test2
